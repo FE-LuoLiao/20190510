a57745af957d2c6378559879a82117c8dd1ff46b
YaWen21 <2656183413@qq.com>
2019-06-08 17:19:53 +0800

    secday

--- src/Components/DataManger/PageNavigation/PageNavigation.jsx
+++ src/Components/DataManger/PageNavigation/PageNavigation.jsx
@@ -14,22 +14,35 @@ export default class PageNavigation extends React.Component {
 
     handleClick(event) {
         this.setState({
-            currentPage: +event.target.dataset.index
+            currentPage: +event.target.dataset.currentPage
         })
-        this.props.getPage(event.target.dataset.index);
-        console.log(event.target.dataset.index)
+        if(event.target.dataset.id == 'right'){
+            
+            this.setState({
+                currentPage:event.target.dataset.currentPage++
+                                                                                                                            });
+        }
+        // if(event.target.dataset.id == 'left'){
+        //     event.target.dataset.currentPage--;
+        // }
+        this.props.getPage(event.target.dataset.currentPage);
+        console.log(event.target.dataset.currentPage)
     }
 
     onMouseO(event) {
-        var event = event.target;
-        if (event.innerText == '...') {
-            event.innerText = '<<';
+        var target = event.target;
+        
+        if (target.innerText == '...' && target.dataset.id == 'left') {
+            target.innerText = '<<';
+        }
+        if (target.innerText == '...' && target.dataset.id == 'right') {
+            target.innerText = '>>';
         }
     }
 
     onMouseL(event) {
         var event = event.target;
-        if (event.innerText == '<<') {
+        if (event.innerText == '<<' || event.innerText == '>>') {
             event.innerText = '...';
         }
     }
@@ -42,7 +55,12 @@ export default class PageNavigation extends React.Component {
                 page.push(2);
                 page.push(3);
                 page.push(4);
-                page.push('...');
+                page.push(
+                    {
+                        id: 'right',
+                        text: '...'
+                    }
+                );
                 page.push(pageNum);
             } else {
                 for (var i = 1; i <= pageNum; i++) {
@@ -50,12 +68,22 @@ export default class PageNavigation extends React.Component {
                 }
             }
         } else {
-            page.push('...');
+            page.push(
+                {
+                    id: 'left',
+                    text: '...'
+                }
+            );
             page.push(currentPage - 1);
             page.push(currentPage);
             page.push(currentPage + 1);
             page.push(currentPage + 2);
-            page.push('....');
+            page.push(
+                {
+                    id: 'right',
+                    text: '...'
+                }
+            );
             page.push(pageNum);
         }
         console.log('currentPage', currentPage);
@@ -84,19 +112,28 @@ export default class PageNavigation extends React.Component {
                     >《</li>
                     {
                         this.pageGen(pagesum, this.state.currentPage).map((item, index) => {
-                            const isCur = this.state.currentPage == item;
+                            const isCur = this.state.currentPage == item;                                                  
+                            let id = item;
+                            let text = item;
+
+                            if(isNaN(item)){
+                                id = item.id;
+                                text = item.text;
+                            }
 
                             return (
-                                <li data-index={item} key={item}
+                                <li data-current-page={id}
+                                    data-id={id}
+                                    key={id}
                                     style={{
                                         backgroundColor: isCur ? '#20a0ff' : '#fff',
                                         color: isCur ? '#fff' : '#333',
                                     }}
-                                    onClick={this.handleClick.bind(this)} onMouseOver={this.onMouse}
-                                    
-                                    // onMouseLeave={this.onMouseL.bind(this)} 
-                                    >
-                                    {item}     
+                                    onClick={this.handleClick.bind(this)}
+                                    onMouseOver={this.onMouseO.bind(this)}
+                                    onMouseLeave={this.onMouseL.bind(this)}
+                                >
+                                    {text}
                                 </li>
                             )
                         }
