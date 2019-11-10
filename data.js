89adc8c0bc2f2cacd36f4cd8366b8b2d87c31187
YaWen21 <2656183413@qq.com>
2019-11-10 17:28:21 +0800

    guowei

--- data.js
+++ data.js
@@ -16,6 +16,7 @@ fs.readFile('./Sanitary_Service_Point.geojson', 'utf-8', function (error, data) 
     const features = JSON.parse(buf.toString()).features;
     if (error) {
         return console.log('读取文件失败，内容是' + error);
+        //try git push
     }
 
     Object.keys(features).forEach(function (key) {
